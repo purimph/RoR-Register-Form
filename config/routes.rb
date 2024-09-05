fb7e69b8e47b7b189933384011dcca22bb32645c
purimph <purim@odds.team>
2024-09-06 00:05:08 +0700

    fix:bug delete button

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :registrations, only: [:index, :new, :create, :edit, :update, :destroy]
+  resources :registrations, only: [:index, :new, :create, :edit, :update, :data, :destroy]
   root 'registrations#new'
 end
 
