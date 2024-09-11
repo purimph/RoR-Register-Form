721e8de6d7ca153c63b69e45b310984ee809f852
purimph <purim@odds.team>
2024-09-11 13:53:49 +0700

    style:make sort a-z in table members and paginate

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,6 +1,6 @@
 class RegistrationsController < ApplicationController
   def index
-    @registrations = Registration.all
+    @registrations = Registration.order(sort_column + " " + sort_direction).paginate(page: params[:page], per_page: 5)
   end
 
   def new
@@ -28,13 +28,23 @@ class RegistrationsController < ApplicationController
       render :edit
     end
   end
+
   def destroy
     @registration = Registration.find(params[:id])
     @registration.destroy
+    alert = "Registration was successfully deleted."
     redirect_to registrations_path, notice: 'Registration was successfully deleted.'
   end
 
   private
+
+  def sort_column
+    Registration.column_names.include?(params[:sort]) ? params[:sort] : "first_name"
+  end
+
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
+  end
 
   def registration_params
     params.require(:registration).permit(:first_name, :last_name, :birthday, :gender, :email, :phone_number, :subject)
